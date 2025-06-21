cc9aa4cb66844eb16e9fbea934379bf981f80de2
malidu04 <malidupahasara04@gmail.com>
2025-06-21 20:56:11 +0530

    server file creation

--- kanban-server/src/server.js
+++ kanban-server/src/server.js
@@ -5,3 +5,19 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const { initSocket } = require('./sockets');
 
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/boards', require('./routes/boards'));
+
+const server = http.createServer(app);
+initSocket(server);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

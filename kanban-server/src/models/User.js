19875c95c28039640adf412e0a808bfa6959a8ea
malidu04 <malidupahasara04@gmail.com>
2025-06-21 23:45:30 +0530

    user model changes

--- kanban-server/src/models/User.js
+++ kanban-server/src/models/User.js
@@ -1,32 +1,45 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, 'Please provide a name'],
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: [true, 'Please provide an email'],
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: 6,
     },
-}, {timestamps: true });
+  },
+  { timestamps: true }
+);
 
+// Only hash password if it's new or modified
 UserSchema.pre('save', async function (next) {
-    if(!this.isModified('password')) {
-        next();
-    }
+  if (!this.isModified('password')) return next();
+
+  try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error); // pass errors to the next handler
+  }
 });
 
-UserSchema.methods.matchPassword = async function (enterPassword) {
-    return await bcrypt.compare(enterPassword, this.password);
+// Match entered password to hashed password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model('User', UserSchema);

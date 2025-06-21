ef8a85978e98084d00b6911ddfb69dc70d88b4d5
malidu04 <malidupahasara04@gmail.com>
2025-06-21 21:04:17 +0530

    server file changes

--- kanban-server/src/server.js
+++ kanban-server/src/server.js
@@ -4,16 +4,27 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const { initSocket } = require('./sockets');
+const errorHandler = require('./middleware/errorMiddleware'); 
+
 
 connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
 
+app.use(cors()); 
+app.use(express.json()); 
+
+// API Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
 app.use('/api/boards', require('./routes/boards'));
+app.use('/api/lists', require('./routes/lists'));
+app.use('/api/cards', require('./routes/cards'));
+
+
+app.use(errorHandler);
+
 
 const server = http.createServer(app);
 initSocket(server);
